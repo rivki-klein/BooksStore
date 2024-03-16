6a009d4698a385cb97f9dbca9d05139907d9a433
rivki klein <rivki0556764827@gmail.com>
2024-03-16 23:07:37 +0200

    Add project files.

---
+++ BooksStoreBL/BookStoreBL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BooksStoreDAL;
+using Entities;
+
+namespace BooksStoreBL
+{
+    public class BookStoreBL
+    {
+        List<BookDetails> ListOfBooks;
+        FileConnection fileConnection;
+        public BookStoreBL()
+        {
+            fileConnection = new FileConnection();
+            ListOfBooks = fileConnection.ReadAllBooks();
+        }
+        //1
+        public IEnumerable<BookDetails> GetBooksOn30()
+        {
+            return ListOfBooks.Where(book => book.Price > 30);
+
+        }
+        //2
+        public IEnumerable<BookDetails> OrderBooks()
+        {
+            return ListOfBooks.OrderBy(book => book.Id);
+
+        }
+        //3
+        public IEnumerable<int> OnlyComics()
+        {
+            return ListOfBooks.Where(book => book.IsComics ==true).Select(book => book.Price);
+        }
+        //4
+        public IEnumerable<string> FitAge9()
+        {
+            return ListOfBooks.Where(book => book.MaxAge <10).Select(book => book.Name);
+        }
+
+        public IEnumerable<BookDetails> GetAllBooks()
+        {
+            return ListOfBooks;
+        }
+    }
+}
---
+++ BooksStoreGUI/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BooksStoreGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BooksStoreBL.BookStoreBL booksStoreBL; 
+        public MainWindow()
+        {
+            booksStoreBL = new BooksStoreBL.BookStoreBL();
+            
+            InitializeComponent();
+            var res = booksStoreBL.GetAllBooks();
+            BooksDataGrid.ItemsSource = res;
+            //1
+            var on30 = booksStoreBL.GetBooksOn30();
+            BooksDataGrid.ItemsSource = on30;
+            //2
+            var sort = booksStoreBL.OrderBooks();
+            BooksDataGrid.ItemsSource = sort;
+            //3
+            var comics = booksStoreBL.OnlyComics();
+            BooksDataGrid.ItemsSource = comics.Select(x => new { Value = x }).ToList();
+            //4
+            var age9 = booksStoreBL.FitAge9();
+            BooksDataGrid.ItemsSource = age9.Select(x => new { Value = x }).ToList();
+        }
+    }
+}
